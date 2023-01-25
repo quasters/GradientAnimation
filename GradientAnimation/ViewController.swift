b0868391befae8aeb667ed0d3a275977d703f5d0
nail.burkeev <nail.burkeev@quantron-systems.com>
2023-01-25 13:39:56 +0300

    Initial Commit

---
+++ GradientAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GradientAnimation
+//
+//  Created by Наиль Буркеев on 25.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
